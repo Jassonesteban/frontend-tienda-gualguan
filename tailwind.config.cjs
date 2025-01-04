4f85bc4f450c347f4104c51d737ba0fa066a68bb
Jasson Gualguan <jgualguanguzman@gmail.com>
2025-01-03 20:22:59 -0500

    Primera version del frontend en REACT

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Esto asegura que Tailwind escanee todos los archivos con código JSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
